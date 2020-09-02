7c44ea93eb79f5fc7e8b154b4575e35a0de31a44
sE7rgo <se7rgo@gmail.com>
2020-09-01 18:25:56 -0700

    resolved conflict, get rid of nesting

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,40 @@
+import { useState, useEffect, createContext, React } from 'react';
+
+import axios from "axios";
+
+//Function to run axios calls, book, edit or cancel an interview
+
+export default function useApplicationData(initial){
+
+  // const [state, setState] = useState({
+  //   park: "Cypress",
+  // });
+
+  const ParkContext = React.createContext("Cypress");
+
+  // const setPark = park => setState({ ...state, park });
+
+  React.useEffect(() => {
+
+    Promise.all([
+      Promise.resolve(axios.get('/api/home')),
+      Promise.resolve(axios.get('/api/parks')),
+      Promise.resolve(axios.get('/api/trails')),
+      Promise.resolve(axios.get('/api/visitors')),
+      Promise.resolve(axios.get('/api/pass')),
+      Promise.resolve(axios.get('/api/mybookings'))
+    ])
+    .then((all) => {
+      setState((prev) => ({
+        ...prev, 
+        home: all[0].data,
+        parks: all[1].data, 
+        trails: all[2].data,
+        visitors: all[3].data,
+        pass: all[4].data,
+        mybooking: all[5].data,
+      }))
+    });
+  }, [])
+  return { ParkContext};
+}
