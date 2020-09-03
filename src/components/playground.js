2532b0cd20cb291428ea2d9e97200b04123067c3
sE7rgo <se7rgo@gmail.com>
2020-09-03 16:36:55 -0700

    small js playground

--- src/components/playground.js
+++ src/components/playground.js
@@ -43,18 +43,19 @@ const visitors = [
   }
 ]
 
-const save = (visitor, visitors) => {
-  const isNotValid = visitors.filter((vis) => {
-    visitor.email === vis.email
-  })
-   if (isNotValid[0] === undefined) {
-     return false
-   } else {
-     visitor.id = visitors.length + 2;
-     visitors.push(visitor)
-     console.log(visitors)
-   }
+const save = (newVisitor, visitors) => {
+  const isNotValid = visitors.filter(visitor => 
+    newVisitor.email === visitor.email
+  )
+  if (!isNotValid[0]) {
+    console.log('nan')
+  } else {
+    newVisitor.id = visitors.length + 2;
+    visitors.push(newVisitor);
+    console.log(visitors);
+  }
 }
+
 let body = {
   id: 0,
   first_name: 'name',
@@ -63,17 +64,5 @@ let body = {
   email: 'andy@email.com',
   password: 'pass'
 }
-// const addNewVisitor = (selectedVisitor, visitors) => {
 
-//   const resultVisitor = data.filter(visitor => 
-//     visitor.email === selectedVisitor.email &&
-//     visitor.password === selectedVisitor.password
-//   ) 
-//   return resultVisitor[0]
-// };
-
-// const body = {
-//   email: 'andy@email.com',
-//   password: 'passwordd'
-// }
 save(body, visitors)
