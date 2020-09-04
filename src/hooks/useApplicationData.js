6114203de3b50cbd92b43e6ec48500d0f411a0c5
Hanna Oh Andrews <hannaohandrews@Dan-PC.edptest.internal>
2020-09-04 15:58:18 -0700

    added json

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,48 +2,6 @@ import { useState, useEffect, createContext, React } from 'react';
 import axios from "axios";
 
 
-<<<<<<< HEAD
-export default function useApplicationData(initial){
-
-  // const [state, setState] = useState({
-  //   park: "Cypress",
-  //   trails: [],
-  //   visitors: {},
-  //   pass: {},
-    
-  // });
-
-  const ParkContext = React.createContext("Cypress");
-
-  // const setPark = park => setState({ ...state, park });
-
-  React.useEffect(() => {
-
-    Promise.all([
-      Promise.resolve(axios.get('/api/parks')),
-      Promise.resolve(axios.get('/api/trails')),
-      Promise.resolve(axios.get('/api/visitors')),
-      Promise.resolve(axios.get('/api/pass_entries')),
-      Promise.resolve(axios.get('/api/mybookings'))
-      Promise.resolve(axios.get('/api/guests'))
-      // Promise.resolve(axios.get('/api/guests?id'))
-    ])
-    .then((all) => {
-      setState((prev) => ({
-        ...prev, 
-        parks: all[0].data, 
-        trails: all[1].data,
-        visitors: all[2].data,
-        pass_entries: all[3].data,
-        mybookings: all[4].data,
-        guests: all[4].data,
-
-      }))
-    });
-  }, [])
-  return { ParkContext};
-}
-=======
 export default function useApplicationData(){
 
   const [state, setState] = useState({
@@ -71,4 +29,3 @@ export default function useApplicationData(){
   
   return { state }
 }
->>>>>>> ad6907f019ca57ea778e5a723b1ce01ea461ec1f
