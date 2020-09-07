aed1682017f1abaeea628328090ea33999f66474
Hanna Oh Andrews <hannaohandrews@Dan-PC.edptest.internal>
2020-09-07 16:59:50 -0700

    added headers

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import ParkContext from "./hooks/ParkContext";
 import DateContext from "./hooks/DateContext";
 import VisitorContext from "./hooks/VisitorContext";
 
-
 // importing components
 import NavBar from './components/NavBar';
 import TrailList from './components/TrailList';
@@ -17,13 +16,14 @@ import Entry from "./components/EntryForm/Index";
 import DateSelector from "./components/DateSelector";
 import Register from "./components/Register";
 import Confirm from './components/Confirm';
+import Slider from './components/Slider';
 // import EntryForm from "components/EntryForm/Index"; 
 
 import "./components/NavBar.scss";
 
-
 const INITIAL = "INITIAL";
 const REGISTER = "REGISTER";
+
 // export default App;
 export default function App() {
 
@@ -41,50 +41,54 @@ export default function App() {
   
   
   return (
-          <DateContext.Provider value={{selectedDate, setSelectedDate}}>
-            <ParkContext.Provider value={{park, setPark}}>
-              <VisitorContext.Provider value={{selectedVisitor, setSelectedVisitor}}>
-                <main className="App">
-                  <nav>
-                    <NavBar
-                      visitors={state.visitors}
-                      onRegister={()=> transition(REGISTER)}
-                    />
-                  </nav>
-                  { mode === INITIAL && (
-                    <div className='main-body'>
-                        <DateSelector></DateSelector>
-                        {/* <Confirm /> */}
-                        <ParksList
-                          parks={state.parks} 
-                          // setPark={setPark}
-                          >
-                          {/* parks={state.parks}  */}
-                          
-                        </ParksList>
-                        { park.name && (
-                          <TrailList 
-                            trails={state.trails}
-                            pass_entries={state.pass_entries}
-                          />
-                        )}
-                        {/* <Entry/> */}
-                    </div>
-                  )}
-                  { mode === REGISTER && (
-                    <Register 
-                      onSetVerify={() => transition(INITIAL)}
+
+    <DateContext.Provider value={{selectedDate, setSelectedDate}}>
+      <ParkContext.Provider value={{park, setPark}}>
+        <VisitorContext.Provider value={{selectedVisitor, setSelectedVisitor}}>
+          <main className="App">
+            <nav>
+              <NavBar
+                visitors={state.visitors}
+                onRegister={()=> transition(REGISTER)}
+              />
+            </nav>
+
+            <Slider/>
+
+            { mode === INITIAL && (
+              <div className='main-body'>
+                  <DateSelector></DateSelector>
+                  {/* <Confirm /> */}
+                  <ParksList
+                    parks={state.parks} 
+                    // setPark={setPark}
+                    >
+                    {/* parks={state.parks}  */}
+                    
+                  </ParksList>
+                  { park.name && (
+                    <TrailList 
+                      trails={state.trails}
+                      pass_entries={state.pass_entries}
                     />
                   )}
+                  {/* <Entry/> */}
+              </div>
+            )}
+            { mode === REGISTER && (
+              <Register 
+                onSetVerify={() => transition(INITIAL)}
+              />
+            )}
 
 
-                  {/* // <h1>{ this.state.message }</h1> */}
-                  {/* // <button onClick={this.fetchData} > */}
-                  {/* //   Fetch Data */}
-                  {/* // </button>         */}
-                </main>
-              </VisitorContext.Provider>
-            </ParkContext.Provider>
-          </DateContext.Provider>
+            {/* // <h1>{ this.state.message }</h1> */}
+            {/* // <button onClick={this.fetchData} > */}
+            {/* //   Fetch Data */}
+            {/* // </button>         */}
+          </main>
+        </VisitorContext.Provider>
+      </ParkContext.Provider>
+    </DateContext.Provider>
   )
 };
